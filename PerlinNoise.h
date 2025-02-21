b2c88de7db10be1582074d98dcea7a43f7419d6a
ukkokalevala <148769692+ukkokalevala@users.noreply.github.com>
2025-02-21 12:49:24 +0200

    Create PerlinNoise.h

---
+++ PerlinNoise.h
@@ -0,0 +1,15 @@
+#ifndef PERLIN_NOISE_H
+#define PERLIN_NOISE_H
+
+#include <cmath>
+
+class PerlinNoise {
+public:
+    float noise(float x, float y) {
+        int n = (int)x + (int)y * 57;
+        n = (n << 13) ^ n;
+        return (1.0 - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);
+    }
+};
+
+#endif
